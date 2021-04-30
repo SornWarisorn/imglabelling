7e7b512499aab13abfadcb5bc6479ec36c28fff7
Sorn <61120048@kmitl.ac.th>
2021-04-30 18:53:18 +0700

    ..

--- src/index.js
+++ src/index.js
@@ -11,22 +11,23 @@ import Create from './Create'
 import DrawAnnotations from './Draw'
 //import { BrowserRouter as Router, useHistory } from 'react-router-dom';
 
-var arr =[];
+
 
 function App() {
 
   const [value, setValue] = useState(0);
+  const [Arr, setArr ] = useState(0);
 
   const handleClick = (e) => {
     setValue(e.target.value)
-    arr.push(e.target.value)
+    setArr(e.target.value)
   }
 
   return (
     <FrameProvider>
       <input type="text"/>
       <button onChange={handleClick}>sdfsfds</button>
-      <input type="text" value={arr} />
+      <input type="text" value={Arr} />
     </FrameProvider>
   );
 }
